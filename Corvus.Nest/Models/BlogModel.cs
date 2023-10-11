a4054ca8542b7fd8c2b0ebef7b537da115f5b4ba
Corvus <Corvus9312@gmail.com>
2023-10-11 10:52:05 +0800

    調整blog list吃json設定檔

---
+++ Corvus.Nest/Models/BlogModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Corvus.Nest.Models;
+
+public class BlogModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Url
+    {
+        get
+        {
+            return $"/Corvus.Nest/blog/{Id}";
+        }
+    }
+
+    public string Description { get; set; } = null!;
+
+    public DateTime CreateTime { get; set; }
+}
---
+++ Corvus.Nest/Pages/Blog.razor.cs
@@ -0,0 +1,26 @@
+﻿using Corvus.Nest.Models;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace Corvus.Nest.Pages;
+
+public class BlogBase : ComponentBase
+{
+    public List<BlogModel>? Blogs { get; set; }
+
+    [Inject]
+    public NavigationManager Navigator { get; set; } = null!;
+
+    [Inject]
+    public HttpClient HttpClient { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var requestUri = Path.Combine(Navigator.BaseUri, "datas", $"blogs.json");
+
+        var res = await HttpClient.GetAsync(requestUri);
+        var json = await res.Content.ReadAsStringAsync();
+
+        Blogs = JsonSerializer.Deserialize<List<BlogModel>>(json);
+    }
+}
