fcc37d04d06aad0f52ce16d92899778fd81d73f7
Corvus <Corvus9312@gmail.com>
2023-10-17 10:57:58 +0800

    add-post11-and-post-footer

--- Corvus.Nest/Pages/BlogPost.razor.cs
+++ Corvus.Nest/Pages/BlogPost.razor.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Corvus.Nest.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using System.Text.Json;
 
 namespace Corvus.Nest.Pages;
 
@@ -17,18 +19,39 @@ public class BlogPostBase : ComponentBase
     [Inject]
     public IJSRuntime JSRuntime { get; set; } = null!;
 
+    public BlogModel? PreviousPost { get; set; }
+
+    public BlogModel? NextPost { get; set; }
+
     protected string PostHtml { get; set; } = string.Empty;
 
-    protected override async Task OnInitializedAsync()
+    private async Task<List<BlogModel>> GetBlogs()
     {
-        var requestUri = Path.Combine(Navigator.BaseUri, "posts", $"post{PostID}.html");
+        var requestUri = Path.Combine(Navigator.BaseUri, "datas", $"blogs.json");
 
         var res = await HttpClient.GetAsync(requestUri);
-        PostHtml = await res.Content.ReadAsStringAsync();
+        var json = await res.Content.ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<List<BlogModel>>(json) ?? new();
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await JSRuntime.InvokeVoidAsync("Prism.highlightAll");
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        var requestUri = Path.Combine(Navigator.BaseUri, "posts", $"post{PostID}.html");
+
+        var res = await HttpClient.GetAsync(requestUri);
+        PostHtml = await res.Content.ReadAsStringAsync();
+
+        var blogs = await GetBlogs();
+
+        PreviousPost = blogs.SingleOrDefault(x => x.Id.Equals(PostID - 1));
+        NextPost = blogs.SingleOrDefault(x => x.Id.Equals(PostID + 1));
+
+        await base.OnParametersSetAsync();
     }
 }
---
+++ Corvus.Nest/Pages/BlogPostFooter.razor.cs
@@ -0,0 +1,13 @@
+﻿using Corvus.Nest.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Corvus.Nest.Pages;
+
+public class BlogPostFooterBase : ComponentBase
+{
+    [Parameter]
+    public BlogModel? PreviousPost { get; set; }
+
+    [Parameter]
+    public BlogModel? NextPost { get; set; }
+}
